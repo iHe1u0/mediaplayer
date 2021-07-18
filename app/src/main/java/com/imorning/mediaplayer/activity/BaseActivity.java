c92ff3e367334e8c75d627e38855a64af94148e4
iMorning <admin@catcompany.cn>
2021-07-18 22:53:23 +0800

    add library for permission

---
+++ app/src/main/java/com/imorning/mediaplayer/activity/BaseActivity.java
@@ -0,0 +1,7 @@
+package com.imorning.mediaplayer.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BaseActivity extends AppCompatActivity {
+
+}
---
+++ app/src/main/java/com/imorning/mediaplayer/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.imorning.mediaplayer.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.imorning.mediaplayer.R;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
